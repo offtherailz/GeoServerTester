fafeecc7b342c53a270caa6a2a7b951619c28d68
Lorenzo Natali <lorenzo.natali@geo-solutions.it>
2013-01-27 19:42:10 +0100

    add javadoc

--- src/main/java/it/geosolutions/geoservertester/Start.java
+++ src/main/java/it/geosolutions/geoservertester/Start.java
@@ -1,3 +1,22 @@
+/**
+ *  Copyright (C) 2007 - 2013 GeoSolutions S.A.S.
+ *  http://www.geo-solutions.it
+ *
+ *  GPLv3 + Classpath exception
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package it.geosolutions.geoservertester;
 
 import it.geosolutions.batchgeocoder.io.Input;
@@ -9,7 +28,11 @@ import java.util.logging.Logger;
 import org.apache.commons.configuration.Configuration;
 import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.PropertiesConfiguration;
-
+/**
+ * Runs a simple test using a GeoServerTester Instance
+ * @author Lorenzo Natali
+ *
+ */
 public class Start {
 	
 	private static Logger LOG = Logger.getLogger(GeoServerTester.class
@@ -24,7 +47,10 @@ public class Start {
 		gst.test(repo.getLocations());
 	}
 
-
+	/**
+	 * Loads the configuration 
+	 * @return the configuration
+	 */
 	private static  Configuration loadConfig() {
 		Configuration conf =null;
 		try {
